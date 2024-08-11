a03589c19edd1a7f689631fad40ce7a97ff2282d
Leo Rondeau <christopherleorondeau@gmail.com>
2024-08-10 21:10:36 -0500

    added backend files/db

---
+++ backend/nashhappsapi/models/event.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Event(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateField()
+    venue = models.ForeignKey('Venue', on_delete=models.CASCADE)
+    band = models.ForeignKey('Band', on_delete=models.CASCADE)
+    time = models.TimeField()
+    creator = models.ForeignKey('Creator', on_delete=models.CASCADE)
