2ecef226da69b756c07e664de95e6a2af86aaa92
Leo Rondeau <christopherleorondeau@gmail.com>
2024-08-11 21:42:25 -0500

    config fixes, removed secret, server responding

--- backend/nashhappsapi/views/localnash.py
+++ backend/nashhappsapi/views/localnash.py
@@ -12,6 +12,8 @@ class TheLocalEventViewSet(viewsets.ViewSet):
     
     @action(detail=False, methods=['get'])
     def fetch_events(self, request):
+        
+        
         url = 'https://localnash.com/calendar/'
         today = datetime.today().strftime('%Y%m%d')
 
@@ -25,26 +27,26 @@ class TheLocalEventViewSet(viewsets.ViewSet):
                 event_titles = event.find_all('h4', class_='mec-event-title')
                 for title_tag in event_titles:
                     event_title = title_tag.text.strip() if title_tag else 'No title found'
-                    event_time = event.find('span', class_='mec-event-time').text.strip()  # Adjust selector
+                    # event_time = event.find('span', class_='mec-event-time').text.strip()  # Adjust selector
 
                     events.append({
                         'date': today,
                         'title': event_title,
-                        'time': event_time,
+                        # 'time': event_time,
                         'description': "Lipsum"
                     })
             
-            venue = Venue.objects.get(name='The Local')
+            # venue = Venue.objects.get(name='The Local')
             # Assuming band names are matched to event titles, adjust as necessary
             for event in events:
                 event_date = datetime.strptime(event['date'], '%Y%m%d').date()
-                event_time = datetime.strptime(event['time'], '%I:%M %p').time()  # Adjust time format if necessary
+                # event_time = datetime.strptime(event['time'], '%I:%M %p').time()  # Adjust time format if necessary
 
                 event_data = {
                     'date': event_date,
-                    'venue': venue.id,
+                    # 'venue': venue.id,
                     'band': Band.objects.get_or_create(name=event['title'])[0].id,
-                    'time': event_time,
+                    # 'time': event_time,
                     # 'creator': creator.id  # Add as needed
                 }
 
@@ -69,10 +71,10 @@ class CreatorSerializer(serializers.ModelSerializer):
         model = Creator
         fields = '__all__'
 
-class VenueSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Venue
-        fields = '__all__'
+# class VenueSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Venue
+#         fields = '__all__'
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
