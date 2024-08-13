af2c01c32843c0c12b7602c9568c2627dacf8c14
Leo Rondeau <christopherleorondeau@gmail.com>
2024-08-12 23:03:20 -0500

    endpoint is returning data and venues are saving to db. Updated event model to make creator optional

--- backend/nashhappsapi/models/event.py
+++ backend/nashhappsapi/models/event.py
@@ -6,4 +6,4 @@ class Event(models.Model):
     venue = models.ForeignKey('Venue', on_delete=models.CASCADE)
     band = models.ForeignKey('Band', on_delete=models.CASCADE)
     time = models.TimeField()
-    creator = models.ForeignKey('Creator', on_delete=models.CASCADE)
+    creator = models.ForeignKey('Creator', on_delete=models.CASCADE, null=True, blank=True)
--- backend/nashhappsapi/views/localnash.py
+++ backend/nashhappsapi/views/localnash.py
@@ -27,27 +27,34 @@ class TheLocalEventViewSet(viewsets.ViewSet):
                 event_titles = event.find_all('h4', class_='mec-event-title')
                 for title_tag in event_titles:
                     event_title = title_tag.text.strip() if title_tag else 'No title found'
-                    # event_time = event.find('span', class_='mec-event-time').text.strip()  # Adjust selector
+                    time_tag = event.find('div', class_='mec-event-time mec-color')
+                    event_time_str = time_tag.text.strip() if time_tag else '00:00 AM'
+                    
+                    event_time = datetime.strptime(event_time_str, '%I:%M %p').time()
 
                     events.append({
                         'date': today,
                         'title': event_title,
-                        # 'time': event_time,
+                        'time': event_time,
                         'description': "Lipsum"
                     })
             
-            # venue = Venue.objects.get(name='The Local')
+            venue, created = Venue.objects.get_or_create(
+                name='The Local',
+                defaults={'website': 'http://www.localnash.com'}
+            )
             # Assuming band names are matched to event titles, adjust as necessary
             for event in events:
                 event_date = datetime.strptime(event['date'], '%Y%m%d').date()
-                # event_time = datetime.strptime(event['time'], '%I:%M %p').time()  # Adjust time format if necessary
+                # time_tag = event.find('div', class_='mec-event-time mec-color')
+                # event_time = time_tag.text.strip() if time_tag else '00:00 AM'
 
                 event_data = {
                     'date': event_date,
-                    # 'venue': venue.id,
+                    'venue': venue.id,
                     'band': Band.objects.get_or_create(name=event['title'])[0].id,
-                    # 'time': event_time,
-                    # 'creator': creator.id  # Add as needed
+                    'time': event_time,
+                    'creator': None
                 }
 
                 serializer = EventSerializer(data=event_data)
@@ -71,10 +78,10 @@ class CreatorSerializer(serializers.ModelSerializer):
         model = Creator
         fields = '__all__'
 
-# class VenueSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Venue
-#         fields = '__all__'
+class VenueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Venue
+        fields = '__all__'
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
