f547d23c138e632a4015f109d3f53c2d11a10e0b
Leo Rondeau <christopherleorondeau@gmail.com>
2024-08-14 22:41:41 -0500

    Removed spotify link from band model, it is not needed. Refactored local fetch to save event to db

--- backend/nashhappsapi/models/band.py
+++ backend/nashhappsapi/models/band.py
@@ -2,4 +2,3 @@ from django.db import models
 
 class Band(models.Model):
     name = models.CharField(max_length=255)
-    spotify = models.URLField()
--- backend/nashhappsapi/views/localnash.py
+++ backend/nashhappsapi/views/localnash.py
@@ -8,66 +8,6 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-class TheLocalEventViewSet(viewsets.ViewSet):
-    
-    @action(detail=False, methods=['get'])
-    def fetch_events(self, request):
-        
-        
-        url = 'https://localnash.com/calendar/'
-        today = datetime.today().strftime('%Y%m%d')
-
-        response = requests.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            calendar_events = soup.find_all('div', attrs={'data-mec-cell': today})
-
-            events = []
-            for event in calendar_events:
-                event_titles = event.find_all('h4', class_='mec-event-title')
-                for title_tag in event_titles:
-                    event_title = title_tag.text.strip() if title_tag else 'No title found'
-                    time_tag = event.find('div', class_='mec-event-time mec-color')
-                    event_time_str = time_tag.text.strip() if time_tag else '00:00 AM'
-                    
-                    event_time = datetime.strptime(event_time_str, '%I:%M %p').time()
-
-                    events.append({
-                        'date': today,
-                        'title': event_title,
-                        'time': event_time,
-                        'description': "Lipsum"
-                    })
-            
-            venue, created = Venue.objects.get_or_create(
-                name='The Local',
-                defaults={'website': 'http://www.localnash.com'}
-            )
-            # Assuming band names are matched to event titles, adjust as necessary
-            for event in events:
-                event_date = datetime.strptime(event['date'], '%Y%m%d').date()
-                # time_tag = event.find('div', class_='mec-event-time mec-color')
-                # event_time = time_tag.text.strip() if time_tag else '00:00 AM'
-
-                event_data = {
-                    'date': event_date,
-                    'venue': venue.id,
-                    'band': Band.objects.get_or_create(name=event['title'])[0].id,
-                    'time': event_time,
-                    'creator': None
-                }
-
-                serializer = EventSerializer(data=event_data)
-                if serializer.is_valid():
-                    serializer.save()
-                else:
-                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
-            return Response(events, status=status.HTTP_200_OK)
-        else:
-            return Response({"error": "Failed to fetch webpage"}, status=status.HTTP_400_BAD_REQUEST)
-
-
 class BandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Band
@@ -87,3 +27,55 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
+
+class TheLocalEventViewSet(viewsets.ViewSet):
+    
+    @action(detail=False, methods=['get'])
+    def fetch_events(self, request):
+        url = 'https://localnash.com/calendar/'
+        today = datetime.today().strftime('%Y%m%d')
+        venue, created = Venue.objects.get_or_create(
+            name='The Local',
+            defaults={'website': 'http://www.localnash.com'}
+        )
+
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            calendar_events = soup.find_all('div', attrs={'data-mec-cell': today})
+
+            events = []
+            for local_event in calendar_events:
+                event_titles = local_event.find_all('h4', class_='mec-event-title')
+                
+                for title_tag in event_titles:
+                    event_title = title_tag.text.strip() if title_tag else 'No title found'
+                    local_band, created = Band.objects.get_or_create(name=event_title)
+                    
+                    time_tag = local_event.find('div', class_='mec-event-time mec-color')
+                    event_time_str = time_tag.text.strip() if time_tag else '00:00 AM'
+                    
+                    try:
+                        event_time = datetime.strptime(event_time_str, '%I:%M %p').time()
+                    except ValueError:
+                        print(f"Error parsing time: {event_time_str}")
+                        event_time = datetime.strptime('00:00 AM', '%I:%M %p').time()
+                    
+                    event_data = {
+                        'date': datetime.today().date(),
+                        'venue': venue.id,
+                        'band': local_band.id,
+                        'time': event_time,
+                        'creator': None  # or the appropriate creator ID
+                    }
+                    
+                    serializer = EventSerializer(data=event_data)
+                    if serializer.is_valid():
+                        event = serializer.save()
+                        events.append(serializer.data)
+                    else:
+                        print(f"Serializer errors: {serializer.errors}")
+            
+            return Response(events, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Failed to fetch webpage"}, status=status.HTTP_400_BAD_REQUEST)
