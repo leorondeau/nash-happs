b0ab4e760ba60065a7ba2fe4cd24e97635347c7b
Leo Rondeau <christopherleorondeau@gmail.com>
2024-11-09 20:11:22 -0600

    added init changes

--- backend/nashhappsapi/views/__init__.py
+++ backend/nashhappsapi/views/__init__.py
@@ -1,7 +1,3 @@
 from .bobbys_ig import fetch_bobbys_events
-<<<<<<< HEAD
 from .localnash import TheLocalEventViewSet
 from .bobbys_ig_azure import fetch_and_validate_instagram_posts
-=======
-from .localnash import TheLocalEventViewSet
->>>>>>> main
